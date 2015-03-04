27239a0afbd6bc59f38355c7e0ed76c3e2cec2ee
Mohit Athwani <mohit@athwani.net>
2015-03-04 22:56:32 +0530

    Initial Commit

---
+++ iBeaconDemo/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  iBeaconDemo
+//
+//  Created by Labs on 3/4/15.
+//  Copyright (c) 2015 Tera Mo Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
